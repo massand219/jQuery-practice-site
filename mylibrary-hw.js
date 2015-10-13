394a70fb231e167e673522cba9063c20fdc851f9
Mehak Massand <massand219@gmail.com>
2015-10-12 22:26:53 -0400

    almost done

--- mylibrary-hw.js
+++ mylibrary-hw.js
@@ -7,9 +7,13 @@ $(document).ready(function(){
 	//slideUp
 	$("h2").slideUp(8000);
 	//Animating CSS:
-	
+	var animateMenu = function(){   
+		$(".Kanye").animate( {opacity: .5}, 7000 ) 
+	} 
+	$(".Kanye").show(animateMenu)
 
-
+	$(".bleh").animate( 
+	{opacity: 0.25, width: "70%"}, 4000 );
 	//click
 	$("h1").click(
 		function(){
