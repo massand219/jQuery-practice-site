cf9c82985fe9a0eaea8f008f7673b7e33b4b31e2
Mehak Massand <massand219@gmail.com>
2015-10-12 22:43:11 -0400

    final

--- mylibrary-hw.js
+++ mylibrary-hw.js
@@ -7,32 +7,18 @@ $(document).ready(function(){
 	//slideUp
 	$("h2").slideUp(8000);
 	//Animating CSS:
+	$(".bleh").animate( 
+	{opacity: 0.25, width: "70%"}, 4000 );
+	//animate with callback:
 	var animateMenu = function(){   
 		$(".Kanye").animate( {opacity: .5}, 7000 ) 
 	} 
 	$(".Kanye").show(animateMenu)
-
-	$(".bleh").animate( 
-	{opacity: 0.25, width: "70%"}, 4000 );
 	//click
 	$("h1").click(
 		function(){
 			alert('you clicked me!');
 		}
 		);
-	// //callbacks:
-	// var bleeBlue = function(){
-	// 	alert("empire state");
-	// }
-	// function jayZ(cripDoe){
-	// 	console.log(4+4);
-	// 	cripDoe();
-	// }
-
-	// jayZ(bleeBlue);
-
-
-
-
 
 })
