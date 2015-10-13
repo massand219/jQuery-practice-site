5c57ea53cbff3e6cd1fe875d347929935e45df38
Mehak Massand <massand219@gmail.com>
2015-10-12 21:06:14 -0400

    initial commit

---
+++ mylibrary-hw.js
@@ -0,0 +1,30 @@
+// jQuery cheat sheet
+$(document).ready(function(){
+	//fadeIn
+	$("p").fadeIn(5000);
+
+
+
+
+	//click
+	$("h1").click(
+		function(){
+			alert('you clicked me!');
+		}
+		);
+	// //callbacks:
+	// var bleeBlue = function(){
+	// 	alert("empire state");
+	// }
+	// function jayZ(cripDoe){
+	// 	console.log(4+4);
+	// 	cripDoe();
+	// }
+
+	// jayZ(bleeBlue);
+
+
+
+
+
+})
